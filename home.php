670fbba7edfc0edd8f4209508e02a31afaa0b2bd
jykiingxuanqi85 <bagusdwiatma2905@gmail.com>
2018-11-26 17:54:05 -0800

    init

---
+++ home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Lumba-lumba</h1>
+	<p>Lumba-Lumba merupakan hewan mamalia yang hidup di air. Dia bernapas menggunakan paru-paru. Lumba-lumba sangat ramah dan baik dengan manusia. Banyak jenis lumba-lumba di bumi kita ini.</p>
+</body>
+</html>
